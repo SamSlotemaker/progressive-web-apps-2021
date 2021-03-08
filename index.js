a4a2651b19512577ec3a0d88a3f9d87519e2277c
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-03-08 13:06:09 +0100

    basic server running

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views/pages"));
+
+
+app.get('/', (req, res) => {
+    res.render('overview')
+})
+
+
+app.listen(port, () => {
+    console.log(`app listening on port: ${port}`)
+})
