a52076e2aff53254c1bf8ce4543a23de422c3601
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-03-13 12:19:29 +0100

    fixed api key

--- src/modules/api/api.js
+++ src/modules/api/api.js
@@ -1,12 +1,13 @@
 const fetch = require("node-fetch")
 const baseURL = 'https://api.rawg.io/api/'
+require('dotenv').config()
 
 module.exports = async function getData(subject, query) {
     try {
         let url = baseURL + subject
         !query || (url += query + '&') //add query to url when given
         query || (url += '?')
-        url += 'key=b7f15a856765429c84087fadd9e167e7'
+        url += 'key=' + process.env.API_KEY
 
         //fetch data if not available local
         const response = await fetch(url)
