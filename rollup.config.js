bfa978dff3883f7606f3f60a5225f834bce9b41c
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-03-26 23:50:38 +0100

    css minify works

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
+import CleanCSS from 'clean-css';
 import { uglify } from "rollup-plugin-uglify";
 import commonjs from '@rollup/plugin-commonjs';
 
@@ -14,9 +15,17 @@ export default [{
             targets: 'dist/*'
         }),
         copy({
-            targets: [{ src: 'src/static/public/*', dest: 'dist/static/public' }]
-        }), copy({
-            targets: [{ src: 'src/views', dest: 'dist' }]
+            targets: [
+                { src: 'src/static/public/scripts/*', dest: 'dist/static/public/scripts' },
+                { src: 'src/static/public/styles/images/*', dest: 'dist/static/public/styles/images' },
+                { src: 'src/static/public/service-worker.js', dest: 'dist/static/public' },
+                { src: 'src/static/public/manifest.json', dest: 'dist/static/public' },
+                { src: 'src/views', dest: 'dist' },
+                {
+                    src: 'src/static/public/styles/style.css',
+                    dest: 'dist/static/public/styles',
+                    transform: (contents) => new CleanCSS().minify(contents).styles,
+                }]
         }),
         commonjs(),
         uglify()
