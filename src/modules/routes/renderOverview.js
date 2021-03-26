a9019767d21b0e828b574afd394e5bed78bfbfe4
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-03-27 00:10:03 +0100

    page query can be used

--- src/modules/routes/renderOverview.js
+++ src/modules/routes/renderOverview.js
@@ -4,9 +4,7 @@ const filterGameList = require('../filter.js')
 
 const subjectGames = 'games'
 const subjectPlatforms = 'platforms'
-const pageSize = '?page_size=20'
-const page = '&page=1'
-const pageQuery = pageSize + page
+const pageSize = '?page_size=10'
 const ordering = '?ordering=-games_count'
 
 //set initial datastate to null
@@ -16,22 +14,27 @@ let platformData = null
 //renders the overview page
 async function renderOverviewPage(req, res) {
     //get data when it doensn't already exist
-    if (!gameData && !platformData) {
-        gameData = await getData(subjectGames, pageQuery)
-        platformData = await getData(subjectPlatforms, ordering)
-        console.log('retrieve data...')
+    let page = '&page='
+    if (req.query.page) {
+        page += req.query.page
+    } else {
+        page += '1'
     }
+    const pageQuery = pageSize + page
+    console.log('getting:' + pageQuery)
+    gameData = await getData(subjectGames, pageQuery)
+    platformData = await getData(subjectPlatforms, ordering)
+    console.log('retrieve data...')
     //create unique genrelist
     const genres = createUniqueGenreList(gameData)
     //extract results from data
     const gameList = gameData.results
-    const platformList = platformData.results
 
     //filter gamelist on what query has been entered, returns original array on empty queries
     let filteredGameList = filterGameList(gameList, req.query.search, req.query.genres)
 
     // render page with the lists
-    res.render('overview', { games: filteredGameList, platforms: platformList, genres: genres, filteredGenre: req.query.genres, searchQuery: req.query.search })
+    res.render('overview', { games: filteredGameList, genres: genres, filteredGenre: req.query.genres, searchQuery: req.query.search })
 }
 
 function sendGamesJSON(req, res) {
